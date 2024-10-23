5a16e7431a3e2c2a8081536990de31accf820547
bmengel7 <brie.mengel@yahoo.com>
2024-10-22 21:47:14 -0400

    commit completed fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,19 @@
-# add your code here
+
+number = 1
+while number <= 100:
+    if (number %3 == 0) and (number %5 == 0):
+        print("FizzBuzz")
+    elif number %3 == 0:
+        print("Fizz")
+    elif (number %5 == 0):
+        print("Buzz")
+    else:
+        print(number)
+    number = number + 1
+
+
+
+
+
+
 
